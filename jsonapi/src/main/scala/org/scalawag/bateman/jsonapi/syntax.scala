ac5c7cbdc90a3287e538fc470306b6000be0432c
Justin Patterson <justin@scalawag.org>
2026-05-13 16:47:27 -0500

    Add .toDocument for Option[Resource] and Seq[Resource]

--- jsonapi/src/main/scala/org/scalawag/bateman/jsonapi/syntax.scala
+++ jsonapi/src/main/scala/org/scalawag/bateman/jsonapi/syntax.scala
@@ -13,12 +13,18 @@
 // limitations under the License.
 
 package org.scalawag.bateman.jsonapi
-
+import cats.syntax.either._
+import cats.syntax.traverse._
+import org.scalawag.bateman.json.syntax._
+import org.scalawag.bateman.json.{JNull, noneIfEmpty}
+import org.scalawag.bateman.jsonapi.encoding.ResourceEncoder.Encoded
 import org.scalawag.bateman.jsonapi.encoding.{
+  DataDocument,
   Document,
   EncodeResult,
   FieldsSpec,
   IncludeSpec,
+  Inclusions,
   InfallibleIncludeSpec,
   ResourceEncoder
 }
@@ -37,5 +43,52 @@ object syntax {
         enc: ResourceEncoder[A]
     ): EncodeResult[Document] =
       enc.encodeResource(a, includeSpec, fieldsSpec).map(_.toDocument)
+  }
+
+  implicit class SeqAnyBatemanJsonApiOps[A](a: Seq[A]) {
+    def toDocument(implicit enc: ResourceEncoder[A]): Document = toDocument()
+
+    private def encodedsToDocument(encodeds: List[Encoded]): Document = {
+      val roots = encodeds.map(_.root)
+      val allInclusions = encodeds.map(_.inclusions).foldLeft(Inclusions.empty)(Inclusions.monoid.combine)
+      DataDocument(data = roots.toJAny, included = noneIfEmpty(allInclusions.objects).map(_.toList))
+    }
+
+    def toDocument(
+        includeSpec: InfallibleIncludeSpec = IncludeSpec.Opportunistically,
+        fieldsSpec: FieldsSpec.Infallible = FieldsSpec.All
+    )(implicit enc: ResourceEncoder[A]): Document = {
+      encodedsToDocument(a.map(enc.encodeInfallibly(_, includeSpec, fieldsSpec)).toList)
+    }
+
+    def toDocument(includeSpec: IncludeSpec, fieldsSpec: FieldsSpec)(implicit
+        enc: ResourceEncoder[A]
+    ): EncodeResult[Document] =
+      a.toList.traverse(enc.encodeResource(_, includeSpec, fieldsSpec)).map(encodedsToDocument)
+  }
+
+  implicit class OptionBatemanJsonApiOps[A](a: Option[A]) {
+    def toDocument(implicit enc: ResourceEncoder[A]): Document = toDocument()
+
+    def toDocument(
+        includeSpec: InfallibleIncludeSpec = IncludeSpec.Opportunistically,
+        fieldsSpec: FieldsSpec.Infallible = FieldsSpec.All
+    )(implicit enc: ResourceEncoder[A]): Document =
+      a match {
+        case Some(s) =>
+          s.toDocument(includeSpec, fieldsSpec)
+        case None =>
+          DataDocument(data = JNull)
+      }
+
+    def toDocument(includeSpec: IncludeSpec, fieldsSpec: FieldsSpec)(implicit
+        enc: ResourceEncoder[A]
+    ): EncodeResult[Document] =
+      a match {
+        case Some(s) =>
+          s.toDocument(includeSpec, fieldsSpec)
+        case None =>
+          DataDocument(data = JNull).asRight
+      }
   }
 }
--- jsonapi/src/test/scala/test/jsonapi/encoding/SyntaxTest.scala
+++ jsonapi/src/test/scala/test/jsonapi/encoding/SyntaxTest.scala
@@ -42,112 +42,390 @@ object SyntaxTest {
 
 class SyntaxTest extends BatemanTestBase with MockFactory {
 
-  it("should encode a document") {
-    new Fixture {
-      mockEncodeResource
-        .expects(instance, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
-        .returns(
-          ResourceEncoder
-            .Encoded(
-              JObject("id" -> "A".toJAny),
-              ResourceObject("", Some("A")),
-            )
-            .asRight
-        )
-        .once()
+  describe("AnyBatemanJsonApiOps") {
 
-      instance.toDocument shouldEncodeTo json"""
-        {
-          "data": {
-            "id": "A"
-          }
-        }
-      """
-    }
-  }
-
-  it("should encode a document with includes") {
-    new Fixture {
-      mockEncodeResource
-        .expects(instance, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
-        .returns(
-          ResourceEncoder
-            .Encoded(
-              JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
-              ResourceObject("T", Some("A")),
-              Inclusions(
-                ResourceObject("T", Some("B")),
-                ResourceObject("T", Some("C")),
+    it("should encode a document") {
+      new Fixture {
+        mockEncodeResource
+          .expects(instance, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("id" -> "A".toJAny),
+                ResourceObject("", Some("A")),
               )
-            )
-            .asRight
-        )
-        .once()
+              .asRight
+          )
+          .once()
 
-      instance.toDocument shouldEncodeTo json"""
-        {
-          "data": { "type": "T", "id": "A" },
-          "included": [
-            { "type": "T", "id": "B" },
-            { "type": "T", "id": "C" }
-          ]
-        }
-      """
+        instance.toDocument shouldEncodeTo json"""
+          {
+            "data": {
+              "id": "A"
+            }
+          }
+        """
+      }
+    }
+
+    it("should encode a document with includes") {
+      new Fixture {
+        mockEncodeResource
+          .expects(instance, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+                Inclusions(
+                  ResourceObject("T", Some("B")),
+                  ResourceObject("T", Some("C")),
+                )
+              )
+              .asRight
+          )
+          .once()
+
+        instance.toDocument shouldEncodeTo json"""
+          {
+            "data": { "type": "T", "id": "A" },
+            "included": [
+              { "type": "T", "id": "B" },
+              { "type": "T", "id": "C" }
+            ]
+          }
+        """
+      }
+    }
+
+    it("should pass infallible arguments through to encoder") {
+      new Fixture {
+        val includeSpec = IncludeSpec.Never
+        val fieldsSpec = FieldsSpec.None
+
+        mockEncodeResource
+          .expects(instance, includeSpec, fieldsSpec, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("id" -> "A".toJAny),
+                ResourceObject("", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+
+        instance.toDocument(includeSpec, fieldsSpec) shouldEncodeTo json"""
+          {
+            "data": {
+              "id": "A"
+            }
+          }
+        """
+      }
+    }
+
+    it("should pass fallible arguments through to encoder") {
+      new Fixture {
+        val includeSpec = IncludeSpec.unsafe("a.b.c,d.e")
+        val fieldsSpec =
+          FieldsSpec(Map("a" -> FieldsSpec.Fields.None, "b" -> FieldsSpec.Fields.Explicit("x")), FieldsSpec.Fields.All)
+
+        mockEncodeResource
+          .expects(*, *, *, *)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("id" -> "A".toJAny),
+                ResourceObject("", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+
+        instance.toDocument(includeSpec, fieldsSpec).map(_.toJObject.render) shouldBe json"""
+          {
+            "data": {
+              "id": "A"
+            }
+          }
+        """.render.asRight
+      }
     }
   }
 
-  it("should pass infallible arguments through to encoder") {
-    new Fixture {
-      val includeSpec = IncludeSpec.Never
-      val fieldsSpec = FieldsSpec.None
+  describe("SeqAnyBatemanJsonApiOps") {
 
-      mockEncodeResource
-        .expects(instance, includeSpec, fieldsSpec, JObject.Empty)
-        .returns(
-          ResourceEncoder
-            .Encoded(
-              JObject("id" -> "A".toJAny),
-              ResourceObject("", Some("A")),
-            )
-            .asRight
-        )
-        .once()
+    it("should encode an empty Seq to an empty data array") {
+      new Fixture {
+        // No mock expectation: encoder must never be called for an empty Seq.
+        Seq.empty[MyClass].toDocument shouldEncodeTo json"""{"data":[]}"""
+      }
+    }
 
-      instance.toDocument(includeSpec, fieldsSpec) shouldEncodeTo json"""
-        {
-          "data": {
-            "id": "A"
+    it("should encode a Seq of one element") {
+      new Fixture {
+        mockEncodeResource
+          .expects(instance, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+
+        Seq(instance).toDocument shouldEncodeTo json"""
+          {
+            "data": [
+              { "type": "T", "id": "A" }
+            ]
           }
-        }
-      """
+        """
+      }
+    }
+
+    it("should encode a Seq of multiple elements") {
+      new Fixture {
+        val instance2 = MyClass()
+        mockEncodeResource
+          .expects(instance, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+        mockEncodeResource
+          .expects(instance2, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "B".toJAny),
+                ResourceObject("T", Some("B")),
+              )
+              .asRight
+          )
+          .once()
+
+        Seq(instance, instance2).toDocument shouldEncodeTo json"""
+          {
+            "data": [
+              { "type": "T", "id": "A" },
+              { "type": "T", "id": "B" }
+            ]
+          }
+        """
+      }
+    }
+
+    it("should merge inclusions across elements") {
+      new Fixture {
+        val instance2 = MyClass()
+        mockEncodeResource
+          .expects(instance, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+                Inclusions(ResourceObject("T", Some("X")))
+              )
+              .asRight
+          )
+          .once()
+        mockEncodeResource
+          .expects(instance2, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "B".toJAny),
+                ResourceObject("T", Some("B")),
+                Inclusions(ResourceObject("T", Some("Y")))
+              )
+              .asRight
+          )
+          .once()
+
+        Seq(instance, instance2).toDocument shouldEncodeTo json"""
+          {
+            "data": [
+              { "type": "T", "id": "A" },
+              { "type": "T", "id": "B" }
+            ],
+            "included": [
+              { "type": "T", "id": "X" },
+              { "type": "T", "id": "Y" }
+            ]
+          }
+        """
+      }
+    }
+
+    it("should pass infallible arguments through to encoder") {
+      new Fixture {
+        val includeSpec = IncludeSpec.Never
+        val fieldsSpec = FieldsSpec.None
+
+        mockEncodeResource
+          .expects(instance, includeSpec, fieldsSpec, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+
+        Seq(instance).toDocument(includeSpec, fieldsSpec) shouldEncodeTo json"""
+          {
+            "data": [
+              { "type": "T", "id": "A" }
+            ]
+          }
+        """
+      }
+    }
+
+    it("should pass fallible arguments through to encoder") {
+      new Fixture {
+        val includeSpec = IncludeSpec.unsafe("a.b.c,d.e")
+        val fieldsSpec =
+          FieldsSpec(Map("a" -> FieldsSpec.Fields.None, "b" -> FieldsSpec.Fields.Explicit("x")), FieldsSpec.Fields.All)
+
+        mockEncodeResource
+          .expects(*, *, *, *)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+
+        Seq(instance).toDocument(includeSpec, fieldsSpec).map(_.toJObject.render) shouldBe json"""
+          {
+            "data": [
+              { "type": "T", "id": "A" }
+            ]
+          }
+        """.render.asRight
+      }
+    }
+
+    it("should propagate encoder errors from any element in fallible mode") {
+      new Fixture {
+        val err = org.scalawag.bateman.jsonapi.encoding.InvalidIncludePath("bogus")
+        mockEncodeResource
+          .expects(*, *, *, *)
+          .returns(cats.data.NonEmptyChain.one(err).asLeft)
+          .once()
+
+        Seq(instance).toDocument(IncludeSpec.unsafe("bogus"), FieldsSpec.All).isLeft shouldBe true
+      }
     }
   }
 
-  it("should pass fallible arguments through to encoder") {
-    new Fixture {
-      val includeSpec = IncludeSpec.unsafe("a.b.c,d.e")
-      val fieldsSpec =
-        FieldsSpec(Map("a" -> FieldsSpec.Fields.None, "b" -> FieldsSpec.Fields.Explicit("x")), FieldsSpec.Fields.All)
+  describe("OptionBatemanJsonApiOps") {
 
-      mockEncodeResource
-        .expects(*, *, *, *)
-        .returns(
-          ResourceEncoder
-            .Encoded(
-              JObject("id" -> "A".toJAny),
-              ResourceObject("", Some("A")),
-            )
-            .asRight
-        )
-        .once()
+    it("should encode None to a null data document") {
+      new Fixture {
+        // Encoder must not be invoked for None.
+        (None: Option[MyClass]).toDocument shouldEncodeTo json"""{"data":null}"""
+      }
+    }
 
-      instance.toDocument(includeSpec, fieldsSpec).map(_.toJObject.render) shouldBe json"""
-        {
-          "data": {
-            "id": "A"
+    it("should encode Some to a single-resource data document") {
+      new Fixture {
+        mockEncodeResource
+          .expects(instance, IncludeSpec.Opportunistically, FieldsSpec.All, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+
+        Some(instance).toDocument shouldEncodeTo json"""
+          {
+            "data": { "type": "T", "id": "A" }
           }
-        }
-      """.render.asRight
+        """
+      }
+    }
+
+    it("should pass infallible arguments through to encoder for Some") {
+      new Fixture {
+        val includeSpec = IncludeSpec.Never
+        val fieldsSpec = FieldsSpec.None
+
+        mockEncodeResource
+          .expects(instance, includeSpec, fieldsSpec, JObject.Empty)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+
+        Some(instance).toDocument(includeSpec, fieldsSpec) shouldEncodeTo json"""
+          {
+            "data": { "type": "T", "id": "A" }
+          }
+        """
+      }
+    }
+
+    it("should produce a null data document in fallible mode for None") {
+      new Fixture {
+        val includeSpec = IncludeSpec.unsafe("a.b")
+        val fieldsSpec = FieldsSpec(Map.empty, FieldsSpec.Fields.All)
+
+        // Encoder must not be invoked for None.
+        (None: Option[MyClass]).toDocument(includeSpec, fieldsSpec).map(_.toJObject.render) shouldBe
+          json"""{"data":null}""".render.asRight
+      }
+    }
+
+    it("should pass fallible arguments through to encoder for Some") {
+      new Fixture {
+        val includeSpec = IncludeSpec.unsafe("a.b.c,d.e")
+        val fieldsSpec =
+          FieldsSpec(Map("a" -> FieldsSpec.Fields.None, "b" -> FieldsSpec.Fields.Explicit("x")), FieldsSpec.Fields.All)
+
+        mockEncodeResource
+          .expects(*, *, *, *)
+          .returns(
+            ResourceEncoder
+              .Encoded(
+                JObject("type" -> "T".toJAny, "id" -> "A".toJAny),
+                ResourceObject("T", Some("A")),
+              )
+              .asRight
+          )
+          .once()
+
+        Some(instance).toDocument(includeSpec, fieldsSpec).map(_.toJObject.render) shouldBe json"""
+          {
+            "data": { "type": "T", "id": "A" }
+          }
+        """.render.asRight
+      }
     }
   }
 
